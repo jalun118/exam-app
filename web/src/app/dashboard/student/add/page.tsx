5537498235f5d745a1751239e44bbfc50e13547d
jalun118 <jalunugroho118@gmail.com>
2024-12-10 22:06:43 +0700

    feat(add student): add main components for single and multiple input to page.tsx

--- web/src/app/dashboard/student/add/page.tsx
+++ web/src/app/dashboard/student/add/page.tsx
@@ -1,3 +1,14 @@
+import ViewInputStudent from "./view-input-student";
+
 export default function AddStudentPage() {
-  return <div>add student</div>;
+  return (
+    <div className="pb-20">
+      <header className="mb-3 dark:border-neutral-700">
+        <h1 className="block text-2xl font-bold text-gray-800 dark:text-white sm:text-3xl">
+          Add Student
+        </h1>
+      </header>
+      <ViewInputStudent />
+    </div>
+  );
 }
